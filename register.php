0a9f542808c400eece24138e9703d6dd649fffc4
Rishabh9877 <86789548+Rishabh9877@users.noreply.github.com>
2021-09-01 13:06:48 +0530

    Update register.php

--- register.php
+++ register.php
@@ -70,29 +70,240 @@
       .brand-name:hover {
         color: #0056b3;
       }
+	     :root {
+    --color-white: hsl(0, 0%, 100%);
+    --color-light: hsl(206, 33%, 96%);
+    --color-black: hsl(0, 0%, 7%);
+    --color-night: hsl(214, 100%, 10%);
+    --color-purple: hsl(291, 64%, 42%);
+    --color-indigo: hsl(255, 100%, 60%);
+    --shadow-small: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    --shadow-medium: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    --shadow-large: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+      0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  }
+
+  html {
+    font-size: 100%;
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+  }
+
+  *,
+  *::before,
+  *::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: inherit;
+    list-style: none;
+    list-style-type: none;
+    text-decoration: none;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+  }
+
+  body {
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    
+  }
+
+  a,
+  button {
+    font-family: inherit;
+    font-size: inherit;
+    line-height: inherit;
+    cursor: pointer;
+    border: none;
+    outline: none;
+    background: none;
+    text-decoration: none;
+  }
+
+  img,
+  video {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    -o-object-fit: cover;
+      object-fit: cover;
+  }
+
+  .container {
+    max-width: 83rem;
+    width: 100%;
+    height: auto;
+    margin: 0 auto;
+    padding: 0 2rem;
+  }
+
+  .brand-link {
+    font-family: inherit;
+    font-size: 1.75rem;
+    font-weight: 700;
+    line-height: inherit;
+    border: none;
+    outline: none;
+    color: var(--color-indigo);
+    text-transform: uppercase;
+    text-rendering: optimizeLegibility;
+  }
+
+  .navbar {
+    max-width: 100%;
+    height: auto;
+    margin: 0 auto;
+    padding: 0.75rem 0;
+    border: none;
+    outline: none;
+    color: var(--color-black);
+    background: var(--color-white);
+    box-shadow: var(--shadow-large);
+  }
+  .navbar .wrapper {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: auto;
+    margin: 0 auto;
+  }
+  .navbar .burger {
+    position: relative;
+    display: none;
+    cursor: pointer;
+    width: 2rem;
+    height: 1rem;
+    border: none;
+    outline: none;
+    opacity: 0;
+    visibility: hidden;
+    background: none;
+    transform: rotate(0deg);
+    transition: 0.35s ease-in-out;
+  }
+  .navbar .burger-line {
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    left: 0;
+    border: none;
+    outline: none;
+    opacity: 1;
+    border-radius: 0.25rem;
+    background: var(--color-black);
+    transform: rotate(0deg);
+    transition: 0.25s ease-in-out;
+  }
+  .navbar .burger-line:nth-child(1) {
+    top: 0;
+  }
+  .navbar .burger-line:nth-child(2), .navbar .burger-line:nth-child(3) {
+    top: 0.5rem;
+  }
+  .navbar .burger-line:nth-child(4) {
+    top: 1rem;
+  }
+  .navbar .burger.active .burger-line:nth-child(1), .navbar .burger.active .burger-line:nth-child(4) {
+    top: 1.25rem;
+    width: 0%;
+    left: 50%;
+  }
+  .navbar .burger.active .burger-line:nth-child(2) {
+    transform: rotate(45deg);
+  }
+  .navbar .burger.active .burger-line:nth-child(3) {
+    transform: rotate(-45deg);
+  }
+  .navbar .menu-inner {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+  }
+  .navbar .menu-link {
+    font-family: inherit;
+    font-size: 1rem;
+    font-weight: 600;
+    line-height: inherit;
+    border: none;
+    color: var(--color-black);
+    text-transform: uppercase;
+    text-rendering: optimizeLegibility;
+    transition: all 0.35s ease-in-out;
+  }
+  .navbar .menu-link.active, .navbar .menu-link:hover {
+    border: none;
+    outline: none;
+    color: var(--color-indigo);
+  }
+
+  @media only screen and (max-width: 768px) {
+    .navbar .burger {
+      display: block;
+      opacity: 1;
+      visibility: visible;
+    }
+    .navbar .menu {
+      width: 100%;
+      max-height: 0rem;
+      padding: 0;
+      opacity: 0;
+      visibility: hidden;
+      overflow: hidden;
+      transition: all 0.35s ease;
+    }
+    .navbar .menu.active {
+      opacity: 1;
+      visibility: visible;
+    }
+    .navbar .menu-inner {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      gap: 0.75rem;
+      padding: 1rem 0;
+    }
+  }
+
     </style>
   </head>
 
   <body style="background-color: rgb(187, 203, 161); font-family: century gothic, Helvetica, sans-serif;">
-    <div id="header">
-      <header class="d-flex flex-wrap justify-content-center pe-3 py-3 mb-4 border-bottom bg-light">
-        <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-          <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-          <h3 class="text-uppercase fw-bold brand-name mt-2">Muetour</h3>
-        </a>
-        <!--navbar-->
-        <nav class="nav-bar nav-pills">
-          <ul class="nav items">
-            <li class="nav-item"><a href="index.php" class="nav-link link-dark" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="index.php#about-us" class="nav-link link-dark">About</a></li>
-            <li class="nav-item"><a href="index.php#services" class="nav-link link-dark">Services</a></li>
-            <li class="nav-item"><a href="index.php#testimonials" class="nav-link link-dark">Testimonial</a></li>
-            <li class="nav-item"><a href="index.php#footer" class="nav-link link-dark">Contact</a></li>
-            <li class="nav-item"><a href="register.php" class="nav-link link-dark active">Register</a></li>
-            <li class="nav-item"><a href="login.php" class="nav-link link-dark">Login</a></li>
-          </ul>
-        </nav>
-      </header>
+     <nav class="navbar">
+        <div class="container">
+          <section class="wrapper">
+            <h1 class="brand"><a href="index.php" class="brand-link">Muetour</a></h1>
+            <button type="button" class="burger" id="burger">
+              <span class="burger-line"></span>
+              <span class="burger-line"></span>
+              <span class="burger-line"></span>
+              <span class="burger-line"></span>
+            </button>
+            <div class="menu" id="menu">
+              <ul class="menu-inner">
+                <li class="menu-item"><a href="index.php" class="menu-link">Home</a></li>
+                <li class="menu-item"><a href="index.php#about-us" class="menu-link">About</a></li>
+                <li class="menu-item"><a href="index.php#services" class="menu-link">Services</a></li>
+                <li class="menu-item"><a href="index.php#testimonials" class="menu-link">Testimonial</a></li>
+                <li class="menu-item"><a href="index.php#footer" class="menu-link">Contact</a></li>
+                <li class="menu-item"><a href="register.php" class="menu-link">Register</a></li>
+                <li class="menu-item"><a href="login.php" class="menu-link">Login</a></li>
+              
+              </ul>
+            </div>
+          </section>
+        </div>
+      </nav>
     </div>
     <form action="register.php" method="post" onsubmit="return pwd_check();">
         <div class="container bg-light my-5 py-4 px-5 rounded">
@@ -169,5 +380,23 @@
       console.log('checking password.', conpwd, pwd);
       return conpwd == pwd;
     }
+      //navbar
+	  
+	    const burgerMenu = document.getElementById("burger");
+  const navbarMenu = document.getElementById("menu");
+
+  // Initialize Responsive Navbar Menu
+  burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    navbarMenu.classList.toggle("active");
+
+    if (navbarMenu.classList.contains("active")) {
+      navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px";
+    } else {
+      navbarMenu.removeAttribute("style");
+    }
+  });
+	  
+
   </script>
 </html>
